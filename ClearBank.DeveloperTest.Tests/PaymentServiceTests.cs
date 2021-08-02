d6499df5f8713cb6f96df31dc59460faf8ce7063
bijo <bijo.punnoose@janushenderson.com>
2021-08-02 12:44:21 +0100

    final commit

---
+++ ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
@@ -0,0 +1,373 @@
+﻿using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    public class PaymentServiceTests
+    {
+        [Fact]
+        public void GetAccountBasedOnDataStoreTypeBackupTest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Bacs
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var accountBackup = ps.GetAccountBasedOnDataStoreType(makePaymentRequest, "Backup");
+
+            //assert
+            accountBackup.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetAccountBasedOnDataStoreTypeNonBackupTest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Bacs
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var accountNonBackup = ps.GetAccountBasedOnDataStoreType(makePaymentRequest, "NonBackup");
+
+            //assert
+            accountNonBackup.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetMakePaymentResultAccountNullTest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Bacs
+            };
+            Account account = null;
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultBacs()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Bacs
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(true);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultBacsAccountNonBacs()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Bacs
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultFasterPaymentsAccountFasterPaymentsAndAccountBalancegreaterThanRequest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.FasterPayments,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments,
+                Balance = 100
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(true);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultFasterPaymentsAccountFasterPaymentsAndAccountBalanceLessThanRequest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.FasterPayments,
+                Amount = 100
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments,
+                Balance = 10
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+        [Fact]
+        public void GetMakePaymentResultFasterPaymentsAccountNonFasterPaymentsAndAccountBalanceGreaterThanRequest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.FasterPayments,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs,
+                Balance = 100
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultChapsAccountChapsAndAccountLive()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps,
+                Status = AccountStatus.Live
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(true);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultChapsAccountNonChapsAndAccountLive()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs,
+                Status = AccountStatus.Live
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+
+        [Fact]
+        public void GetMakePaymentResultChapsAccountChapsAndAccountNonLive()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps,
+                Status = AccountStatus.Disabled
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.GetMakePaymentResult(makePaymentRequest, account);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+
+        [Fact]
+        public void UpdateAccountMakePaymentResultFailure()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10                
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps,
+                Status = AccountStatus.Disabled
+            };
+            var makePaymentResult = new MakePaymentResult()
+            {
+                Success = false
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            Action act = () => ps.UpdateAccount(makePaymentRequest, "Backup", account, null);
+
+            //assert
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage("Make Payment not successful. MakePaymentResult did not return success");
+        }
+
+        [Fact]
+        public void UpdateAccountMakePaymentResultSuccess()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            //Setuppayment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10
+            };
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps,
+                Status = AccountStatus.Disabled
+            };
+            var makePaymentResult = new MakePaymentResult()
+            {
+                Success = true
+            };
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            Action act = () => ps.UpdateAccount(makePaymentRequest, "Backup", account, makePaymentResult);
+
+            //assert
+            act.Should().NotThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void MakePaymentRequest()
+        {
+            //arrange
+            //Mock Iconfiguration
+            var configuration = new Mock<IConfiguration>();
+            var configurationSection = new Mock<IConfigurationSection>();
+            configurationSection.Setup(a => a.Value).Returns("Backup");
+            configuration.Setup(a => a.GetSection("DataStoreType")).Returns(configurationSection.Object);
+            //Setup payment service
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.Chaps,
+                Amount = 10
+            };
+
+            /*
+            //We could mock GetAccountBasedOnDataStoreTypecif GetAccount can be injected into MakePayment method (ie Dependency Inversion Principle)
+            Account account = new Account
+            {
+                AllowedPaymentSchemes = AllowedPaymentSchemes.Chaps,
+                Status = AccountStatus.Disabled
+            };
+            var mPS = new Mock<IPaymentService>();
+            mPS.Setup(x => x.GetAccountBasedOnDataStoreType(makePaymentRequest,"Backup")).Returns(account);
+            */
+
+            //act
+            var ps = new PaymentService(configuration.Object);
+            var result = ps.MakePayment(makePaymentRequest);
+
+            //assert
+            result.Success.Should().Be(false);
+        }
+    }
+}
---
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -0,0 +1,114 @@
+﻿using ClearBank.DeveloperTest.Data;
+using ClearBank.DeveloperTest.Types;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ClearBank.DeveloperTest.Tests")]
+namespace ClearBank.DeveloperTest.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+
+        //Added an example of DI.
+        public PaymentService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        internal void UpdateAccount(MakePaymentRequest request, string dataStoreType, Account account, MakePaymentResult result)
+        {
+            try
+            {
+                if (result.Success)
+                {
+                    account.Balance -= request.Amount;
+
+                    if (dataStoreType == "Backup")
+                    {
+                        var accountDataStore = new BackupAccountDataStore();
+                        accountDataStore.UpdateAccount(account);
+                    }
+                    else
+                    {
+                        var accountDataStore = new AccountDataStore();
+                        accountDataStore.UpdateAccount(account);
+                    }
+                }
+            }
+            catch
+            {
+                throw new InvalidOperationException("Make Payment not successful. MakePaymentResult did not return success");
+            }
+        }
+
+        internal MakePaymentResult GetMakePaymentResult(MakePaymentRequest request, Account account)
+        {
+            var result = new MakePaymentResult();
+            if (account == null)
+            {
+                result.Success = false;
+                return result;
+            }
+
+            switch (request.PaymentScheme)
+            {
+                case PaymentScheme.Bacs:
+                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
+                    {
+                        result.Success = false;
+                        return result;
+                    }
+                    break;
+
+                case PaymentScheme.FasterPayments:
+                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments)
+                        || account.Balance < request.Amount)
+                    {
+                        result.Success = false;
+                        return result;
+                    }
+                    break;
+
+                case PaymentScheme.Chaps:
+                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps) ||
+                        account.Status != AccountStatus.Live)
+                    {
+                        result.Success = false;
+                        return result;
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        //This method should be ideally extracted to a separate class and injected into MakePayment method
+        //This would help in mocking this method and better unit testing of MakePayment
+        //Commented code in the unit test project line 354-363
+        public Account GetAccountBasedOnDataStoreType(MakePaymentRequest request, string dataStoreType)
+        {
+            if (dataStoreType == "Backup")
+            {
+                var backupAccountDataStore = new BackupAccountDataStore();
+                return backupAccountDataStore.GetAccount(request.DebtorAccountNumber);
+            }
+
+            var accountDataStore = new AccountDataStore();
+            return accountDataStore.GetAccount(request.DebtorAccountNumber);
+        }
+
+        public MakePaymentResult MakePayment(MakePaymentRequest request)
+        {
+            var dataStoreType= _configuration.GetValue<string>("DataStoreType");
+
+            var account = GetAccountBasedOnDataStoreType(request, dataStoreType);
+
+            MakePaymentResult result = GetMakePaymentResult(request, account);
+
+            UpdateAccount(request, dataStoreType, account, result);
+
+            return result;
+        }
+    }
+}
